0d5b352535a7e0f4dacf93fb4afbb894e7eb8dde
Sanket Bhandare <sanket.s.bhandare@gmail.com>
2022-05-28 19:57:32 +0530

    changes updated

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const numbers = [1,2,3,4,5,6,7,8,9];
+createDom(numbers);
+var htmlContent = '';
 
-const obj = [
-    {Number : 1 , color : "#6F98A8" },
-    {Number : 2 , color : "#2B8EAD" },
-    {Number : 3 , color : "#72C3DC" },
-    {Number : 4 , color : "#6F98A8" },
-]
-document.getElementById("demo1").innerHTML = numbers;
 function sortNum(){
     numbers.sort();
-    document.getElementById("demo1").innerHTML = numbers;
+    createDom(numbers);
 };
 function shufNum(){
+    htmlContent = '';
     function shuffleArray(numbers) {
         numbers.sort(() => Math.random() - 0.5);
       }
       shuffleArray(numbers);
-      document.getElementById("demo1").innerHTML = numbers;
-    //   numbers.forEach(el => {
-    //     document.getElementById('demo1').innerHTML += `<div class="col-4">${el}</div><br />`;
-    //  });
+      createDom(numbers);
 };
+
+function createDom(numbers) {
+    htmlContent = '';
+    for(var i = 0; i < numbers.length; i++) {
+        htmlContent += "<div class='block bg" + numbers[i] +"'>"+"<span>" + numbers[i] + "</span>"+"</div>";
+        document.getElementById("demo1").innerHTML = htmlContent;
+    }
+}
